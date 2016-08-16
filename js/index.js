05dc9b46c101e3d2b4f10838b3402db12d47c634
Anirudh Goel <Anirudhgoel.delhi@gmail.com>
2016-08-17 01:49:29 +0530

    made input fields more flexible

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
-function pinThem() {
+// https://in.pinterest.com/anirudhgoel/die/
+
+function pinThem(event) {
+	event.preventDefault();
+
 	var source_board = $("#source_board").val();
 	var destination_board = $("#destination_board").val();
-	alert("Done" + source_board + destination_board);
+	
+	if (source_board.substring(0,6) == "https:") {
+		var source_board_parts = source_board.split("/");
+		var source_board = source_board_parts[3] + "/" + source_board_parts[4];
+	} else if (source_board.replace(/^\/|\/$/g, "").split("/").length == 2) {
+		source_board = source_board.replace(/^\/|\/$/g, "");
+	} else {
+		$("err1").html("");
+	}
+
+	if (destination_board.substring(0,6) == "https:") {
+		var destination_board_parts = destination_board.split("/");
+		var destination_board = destination_board_parts[3] + "/" + destination_board_parts[4];
+	} else if (destination_board.replace(/^\/|\/$/g, "").split("/").length == 2) {
+		destination_board = destination_board.replace(/^\/|\/$/g, "");
+	} else {
+		$("err2").html("");
+	}
+
+	alert(source_board);
+
+	// alert("Done" + source_board + destination_board);
 }
